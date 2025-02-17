61b02daef61288527b01ed7ac2b8cf8f69285593
Serge Zaugg <serge.zaugg@gmail.com>
2025-02-17 08:55:23 +0100

    training procedure

---
+++ 01_train_aec.py
@@ -0,0 +1,171 @@
+#----------------------
+#
+#
+#----------------------
+
+import plotly.express as px
+import torch
+import plotly.express as px
+import numpy as np
+from torchsummary import summary
+import torch.nn as nn
+import torch.optim as optim
+import os 
+from torchvision.transforms.functional import pil_to_tensor
+from data_utils import SpectroImageDataset, Encoder, Decoder
+
+torch.cuda.is_available()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+
+
+imgpath    = "C:/xc_real_projects/xc_aec_project/downloaded_data_img_24000sps"
+model_path = "C:/xc_real_projects/models"
+
+
+n_epochs = 15
+
+#----------------------
+# define data loader 
+train_dataset = SpectroImageDataset(imgpath)
+train_dataset.__len__()
+xx = train_dataset.__getitem__(45)
+xx
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32,  shuffle=True)
+for i, (data, fi) in enumerate(train_loader, 0):
+    if i > 3:
+        break
+    print(data.shape)
+
+
+
+
+#----------------------
+# define models 
+
+impsha = (128, 128)
+latsha = 256
+n_blck = 5
+
+model_enc = Encoder(n_ch_in=3, 
+                    n_ch_latent=latsha, 
+                    shape_input = impsha, 
+                    n_conv_blocks = n_blck,
+                    ch = [32, 64, 96, 128, 196],
+                    po = [(4, 4), (4, 4), (2, 2), (2, 2), (2, 2)]
+                    ) 
+model_enc = model_enc.to(device)
+summary(model_enc, (3, 128, 128))
+
+model_dec = Decoder(n_ch_out=3, 
+                    n_ch_latent=latsha, 
+                    shape_output = impsha, 
+                    n_conv_blocks = n_blck,
+                    ch = [196, 128, 96, 64, 32],
+                    po = [(2, 2), (2, 2), (2, 2), (2, 2), (2, 2)]
+                    )
+model_dec = model_dec.to(device)
+summary(model_dec, (latsha,))
+
+
+
+
+
+
+
+# instantiate loss, optimizer
+criterion = nn.MSELoss() #nn.BCELoss()
+optimizer = optim.Adam(list(model_enc.parameters()) + list(model_dec.parameters()), lr=0.001)
+
+
+# # instantiate loss, optimizer
+# criterion = nn.MSELoss(reduction='mean')
+# optimizer = optim.Adam(list(model_enc.parameters()) + list(model_dec.parameters()), lr=0.001)
+
+
+
+_ = model_enc.train()
+_ = model_dec.train()
+
+# initialize the best validation loss as infinity
+best_val_loss = float("inf")
+# start training by looping over the number of epochs
+for epoch in range(n_epochs):
+    print(f"Epoch: {epoch + 1}/{n_epochs}")
+    # set the encoder and decoder models to training mode
+   
+    for btchi, (data, fi) in enumerate(train_loader, 0):
+        # print(data.shape)
+        data = data.to(device)
+        # data.shape
+        # data.dtype
+        # reset the gradients 
+        optimizer.zero_grad()
+        # forward 
+        encoded = model_enc(data).to(device)
+        # encoded.max()
+        # encoded.shape
+        decoded = model_dec(encoded).to(device)
+        # decoded.max()
+        # decoded.shape
+        # compute the reconstruction loss 
+        loss = criterion(decoded, data)
+        # compute the gradients
+        loss.backward()
+        # update the weights
+        optimizer.step()
+        # accumulate the loss 
+    print('loss', loss.item())
+    print('data min max',  data.min(), data.max())
+    print('decoded min max', decoded.min(), decoded.max()) 
+    
+    
+
+
+# save models
+# torch.save(model_enc, os.path.join(model_path, "tensor.pkl"))
+
+
+# Save the model
+torch.save(model_enc.state_dict(), os.path.join(model_path, "encoder_model.pth") )
+
+# torch.save(model.state_dict(), PATH)
+
+
+
+
+
+
+
+
+
+# check reconstruction with examples 
+if False:
+
+    data = data.to(device)
+    encoded = model_enc(data).to(device)
+    decoded = model_dec(encoded).to(device)
+
+    # ii = 489 
+    for ii in range(10):
+        img_orig = data[ii].cpu().numpy()
+        img_orig = img_orig.transpose(1,2,0)
+        img_orig.min()
+        img_orig.max()
+        img_orig.dtype
+        fig00 = px.imshow(img_orig, height = 500, title="original")
+        fig00.show()
+
+        img_reco = decoded[ii].cpu().detach().numpy()
+        img_reco = img_reco.transpose(1,2,0)
+        img_reco = 255*(img_reco - img_reco.min())/(img_reco.max())
+        img_reco.min()
+        img_reco.max()
+        img_reco.dtype
+        fig01 = px.imshow(img_reco, height = 500, title="reconstructed")
+        fig01.show()
+
+
+
+
