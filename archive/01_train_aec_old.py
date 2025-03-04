69f0637d5c51ca0641d22e590634a23b8ae57cf1
Serge Zaugg <serge.zaugg@gmail.com>
2025-03-04 09:32:07 +0100

    misc

--- archive/01_train_aec_old.py
+++ archive/01_train_aec_old.py
@@ -10,13 +10,15 @@ from torchsummary import summary
 import torch.nn as nn
 import torch.optim as optim
 import os 
+from custom_models_old import Encoder, Decoder, SpectroImageDataset
 import datetime
-from custom_models import Encoder, Decoder, SpectroImageDataset
-# from custom_models_old import Encoder, Decoder, SpectroImageDataset
-import pickle
+
 
 torch.cuda.is_available()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+
 
 imgpath_train = "C:/xc_real_projects/xc_aec_project_n_europe/downloaded_data_img_24000sps"
 imgpath_test  = "C:/xc_real_projects/xc_aec_project_n_europe/downloaded_data_img_24000sps"
@@ -39,89 +41,39 @@ for i, (data, fi) in enumerate(train_loader, 0):
 
 n_batches = train_dataset.__len__() // batch_size
 
+# test 
+test_dataset = SpectroImageDataset(imgpath_test)
+test_loader  = torch.utils.data.DataLoader(test_dataset, batch_size=64,  shuffle=True, drop_last=True)
 
 
 
 
 #----------------------
 # define models 
-if True:
 
-    par = { 
-        'n_blck' : 5,
-        'e': {
-            'n_ch_in' : 1,
-            'ch' : [64, 128, 256, 256, 256],
-            'po' : [(2, 2), (2, 2), (4, 2), (4, 2), (2, 2)],
-            },
-        'd': {
-            'n_ch_out' : 1,
-            'ch' :  [256, 256, 256, 128, 64],
-            'po' :  [(2, 2), (2, 2), (4, 2), (2, 2), (2, 2)],
-            }}
+impsha = (128, 128)
+latsha = 256
+n_blck = 3
 
-    model_enc = Encoder(n_ch_in = par['e']['n_ch_in'], 
-                        n_conv_blocks = par['n_blck'],
-                        ch = par['e']['ch'],
-                        po = par['e']['po']
-                        ) 
+model_enc = Encoder(n_ch_in = 1, 
+                    n_ch_latent=latsha, 
+                    shape_input = impsha, 
+                    n_conv_blocks = n_blck,
+                    ch = [16, 32, 64, 256, 512],
+                    po = [(2, 2), (2, 2), (2, 2), (2, 2), (2, 2)]
+                    ) 
+model_enc = model_enc.to(device)
+summary(model_enc, (1, impsha[0], impsha[1]))
 
-    model_enc = model_enc.to(device)
-    summary(model_enc, (1, 128, 128))
-
-
-
-
-    model_dec = Decoder(n_ch_out = par['d']['n_ch_out'], 
-                        ch = par['d']['ch'], 
-                        po = par['d']['po'], 
-                        )
-
-    model_dec = model_dec.to(device)
-    summary(model_dec, (256, 1, 4))
-
-
-
-
-#old models 
-
-if False:
-
-    par = { 
-        'n_blck' : 3,
-        'e': {
-            'n_ch_in' : 1,
-            },
-        'd': {
-            'n_ch_out' : 1,
-            }}
-
-    impsha = (128, 128)
-    latsha = 256
-
-    model_enc = Encoder(n_ch_in = par['e']['n_ch_in'], 
-                        n_ch_latent=latsha, 
-                        shape_input = impsha, 
-                        n_conv_blocks = par['n_blck'],
-                        ch = [16, 32, 64, 256, 512],
-                        po = [(2, 2), (2, 2), (2, 2), (2, 2), (2, 2)]
-                        ) 
-    model_enc = model_enc.to(device)
-    summary(model_enc, (1, impsha[0], impsha[1]))
-
-
-    model_dec = Decoder(n_ch_out = par['d']['n_ch_out'], 
-                        n_ch_latent=latsha, 
-                        shape_output = impsha, 
-                        n_conv_blocks = par['n_blck'],
-                        ch = [128, 64, 64, 32, 32],
-                        po = [(2, 2), (2, 2), (2, 2), (2, 2), (2, 2)]
-                        )
-    model_dec = model_dec.to(device)
-    summary(model_dec, (latsha,))
-
-
-
+model_dec = Decoder(n_ch_out = 1, 
+                    n_ch_latent=latsha, 
+                    shape_output = impsha, 
+                    n_conv_blocks = n_blck,
+                    ch = [128, 64, 64, 32, 32],
+                    po = [(2, 2), (2, 2), (2, 2), (2, 2), (2, 2)]
+                    )
+model_dec = model_dec.to(device)
+summary(model_dec, (latsha,))
 
 
 
@@ -130,6 +82,7 @@ if False:
 
 # --------------------------------
 # train 
+
 # torch.optim.Adam(params, lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0, 
 #                  amsgrad=False, *, foreach=None, maximize=False, capturable=False, 
 #                  differentiable=False, fused=None)
@@ -137,12 +90,17 @@ if False:
 # instantiate loss, optimizer
 criterion = nn.MSELoss() #nn.BCELoss()
 optimizer = optim.Adam(list(model_enc.parameters()) + list(model_dec.parameters()), lr=0.001)
+# optimizer = optim.Adam(list(model_enc.parameters()) + list(model_dec.parameters()), lr=0.0001)
 # optimizer = optim.SGD(list(model_enc.parameters()) + list(model_dec.parameters()), lr=0.01, momentum=0.9)
 
 _ = model_enc.train()
 _ = model_dec.train()
 
 n_epochs = 1
+
+# initialize the best validation loss as infinity
+best_val_loss = float("inf")
+# start training by looping over the number of epochs
 
 loss_li_train = []
 loss_li_test  = []
@@ -153,9 +111,6 @@ for epoch in range(n_epochs):
     loss_tra =[]
     loss_tes =[]
     for btchi, (data, fi) in enumerate(train_loader, 0):
-
-        if btchi > 500:
-            break
         # print(btchi)
         # print(data.shape)
         data = data.to(device)
@@ -174,26 +129,12 @@ for epoch in range(n_epochs):
         optimizer.step()
         # accumulate the loss 
 
-        if btchi % 10 == 0:
-            print('loss', np.round(loss.item(),5), "   --- status: "  + str(btchi) + " out of " + str(n_batches) + " batches")
-            print(data.cpu().detach().numpy().min().round(3) , data.cpu().detach().numpy().max().round(3) )
-            print(decoded.cpu().detach().numpy().min().round(3) , decoded.cpu().detach().numpy().max().round(3) )
-            print("-")
-
-  
-    
+        print('loss', np.round(loss.item(),5))
+     
 # Save the model
-tstmp = datetime.datetime.now().strftime("_%Y%m%d_%H%M%S")
-
-model_save_name = "encoder_model"+tstmp+f"_epo_{epoch + 1}" +  ".pth"
+datetimestamp = datetime.datetime.now().strftime("_%Y%m%d_%H%M%S")
+model_save_name = "encoder_model"+f"_epo_{epoch + 1}" + f"_nlat_{latsha }" + f"_nblk_{n_blck }" ".pth"
 torch.save(model_enc.state_dict(), os.path.join(model_path, model_save_name))
-
-model_save_name = "decoder_model"+tstmp+f"_epo_{epoch + 1}" + ".pth"
-torch.save(model_dec.state_dict(), os.path.join(model_path, model_save_name))
-
-param_save_name = "params_model"+tstmp+f"_epo_{epoch + 1}" + ".json"
-with open(os.path.join(model_path, param_save_name), 'wb') as fp:
-    pickle.dump(par, fp)
 
 
 
@@ -208,7 +149,7 @@ if False:
     data.shape
 
     # ii = 489 
-    for ii in np.random.randint(data.shape[0], size = 5):
+    for ii in np.random.randint(data.shape[0], size = 10):
         img_orig = data[ii].cpu().numpy()
         img_orig.shape
         # img_orig = img_orig.transpose(1,2,0) # 3 ch
@@ -229,8 +170,5 @@ if False:
         img_reco.dtype
         fig01 = px.imshow(img_reco, height = 500, title="reconstructed")
         fig01.show()
-
-
-
 
 
---
+++ custom_models_old.py
@@ -0,0 +1,346 @@
+#--------------------------------
+# A collection of two classes from nn.Module to initialize convolutional encoders and decoders.
+# The __init__ method has several parameter for fine control of the architecture
+#--------------------------------
+
+import numpy as np
+import torch
+import numpy as np
+from torchsummary import summary
+import torch.nn as nn
+import torch
+from torch.utils.data import Dataset
+import os 
+from torchvision.transforms.functional import pil_to_tensor
+from PIL import Image
+import torch
+import numpy as np
+import torch.nn as nn
+from torch.utils.data import Dataset
+import os 
+import torchvision.transforms as transforms
+from torchsummary import summary
+
+torch.cuda.is_available()
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+
+dataaugm = transforms.Compose([
+    transforms.RandomAffine(degrees=(-1.0, 1.0), translate=(0.02,0.02), scale=(0.98,1.02)),
+    # transforms.RandomCrop(size=126, padding=None, pad_if_needed=False)
+    ])
+
+# Create a dataset for png images in a folder
+class SpectroImageDataset(Dataset):
+    def __init__(self, imgpath):
+        self.all_img_files = [a for a in os.listdir(imgpath) if '.png' in a]
+        self.imgpath = imgpath
+    def __getitem__(self, index):     
+        img = Image.open( os.path.join(self.imgpath,  self.all_img_files[index] ))
+        # img = dataaugm(img)
+        # img = img.resize((128, 128))
+        x = pil_to_tensor(img).to(torch.float32) / 255.0
+        y = self.all_img_files[index]
+        return (x ,y)
+    def __len__(self):
+        return (len(self.all_img_files))
+
+
+
+
+
+class Encoder(nn.Module):
+
+    def __init__(self, n_ch_in, n_ch_latent, shape_input, n_conv_blocks, padding = "same",
+                 ch = [16, 32, 64, 128, 256], po = [(2, 2), (2, 2), (2, 2), (2, 2), (2, 2)]):
+        """
+        Initialize a convolutional encoder (first half of an auto-encoder)
+        Parameters:
+        n_ch_in (int): nb channels in input image
+        n_ch_latent (int): size of the latent vector
+        shape_input (list): width an height of the input image, e.g. [32,32]
+        n_conv_blocks (int): number of convolutional blocks, should be in 0 to 5
+        padding (str): set to "same"
+        ch (list): Nb of channels in each convolutional block, list of length n_conv_blocks
+        po (list): Pooling applied after each convolutional block, list of length n_conv_blocks
+        """
+        
+        super(Encoder, self).__init__()
+
+        self.shi = shape_input
+        self.padding = padding
+        self.n_conv_blocks = n_conv_blocks
+
+        # conv block 0
+        if self.n_conv_blocks >= 1:
+            self.conv0 = nn.Sequential(
+                nn.Conv2d(n_ch_in,  ch[0], kernel_size=(3,3), stride=1, padding=self.padding),
+                nn.Conv2d(ch[0], ch[0], kernel_size=(3,3), stride=1, padding=self.padding),
+                nn.BatchNorm2d(ch[0]),
+                nn.ReLU(),
+                nn.AvgPool2d(po[0], stride=po[0])
+                )
+        else:
+            self.conv0 = nn.Identity()
+
+        # conv block 1
+        if self.n_conv_blocks >= 2:
+            self.conv1 = nn.Sequential(
+                nn.Conv2d(ch[0], ch[1], kernel_size=(3,3), stride=1, padding=self.padding),
+                nn.Conv2d(ch[1], ch[1], kernel_size=(3,3), stride=1, padding=self.padding),
+                nn.BatchNorm2d(ch[1]),
+                nn.ReLU(),
+                nn.AvgPool2d(po[1], stride=po[1])
+                )
+        else:
+            self.conv1 = nn.Identity()
+        
+        # conv block 2
+        if self.n_conv_blocks >= 3:
+            self.conv2 = nn.Sequential(
+                nn.Conv2d(ch[1], ch[2], kernel_size=(3,3), stride=1, padding=self.padding),
+                nn.Conv2d(ch[2], ch[2], kernel_size=(3,3), stride=1, padding=self.padding),
+                nn.BatchNorm2d(ch[2]),
+                nn.ReLU(),
+                nn.AvgPool2d(po[2], stride=po[2])
+                )
+        else:
+            self.conv2 = nn.Identity()
+
+        # conv block 3
+        if self.n_conv_blocks >= 4:
+            self.conv3 = nn.Sequential(
+                nn.Conv2d(ch[2], ch[3], kernel_size=(3,3), stride=1, padding=self.padding),
+                nn.Conv2d(ch[3], ch[3], kernel_size=(3,3), stride=1, padding=self.padding),
+                nn.BatchNorm2d(ch[3]),
+                nn.ReLU(),
+                nn.AvgPool2d(po[3], stride=po[3])
+                )
+        else:
+            self.conv3 = nn.Identity()    
+
+        # conv block 4
+        if self.n_conv_blocks >= 5:
+            self.conv4 = nn.Sequential(
+                nn.Conv2d(ch[3], ch[4], kernel_size=(3,3), stride=1, padding=self.padding),
+                nn.Conv2d(ch[4], ch[4], kernel_size=(3,3), stride=1, padding=self.padding),
+                nn.BatchNorm2d(ch[4]),
+                nn.ReLU(),
+                nn.AvgPool2d(po[4], stride=po[4])
+                )
+        else:
+            self.conv4 = nn.Identity()    
+
+        self.flatn = nn.Flatten()
+
+        # flattened_size = int(self.ch1 * (self.shi[0]/(self.pool0[0]*self.pool1[0])) * (self.shi[1]/(self.pool0[1]*self.pool1[1])))
+        if self.n_conv_blocks == 0:
+            flattened_size = int(n_ch_in * (self.shi[0]) * (self.shi[1]))
+        if self.n_conv_blocks == 1:
+            flattened_size = int(ch[0] * (self.shi[0]/(po[0][0])) * (self.shi[1]/(po[0][1])))
+        if self.n_conv_blocks == 2:
+            flattened_size = int(ch[1] * (self.shi[0]/(po[0][0]*po[1][0])) * (self.shi[1]/(po[0][1]*po[1][1])))
+        if self.n_conv_blocks == 3:
+            flattened_size = int(ch[2] * (self.shi[0]/(po[0][0]*po[1][0]*po[2][0])) * (self.shi[1]/(po[0][1]*po[1][1]*po[2][1]))) 
+        if self.n_conv_blocks == 4:
+            flattened_size = int(ch[3] * (self.shi[0]/(po[0][0]*po[1][0]*po[2][0]*po[3][0])) * (self.shi[1]/(po[0][1]*po[1][1]*po[2][1]*po[3][1]))) 
+        if self.n_conv_blocks == 5:
+            flattened_size = int(ch[4] * (self.shi[0]/(po[0][0]*po[1][0]*po[2][0]*po[3][0]*po[4][0])) * (self.shi[1]/(po[0][1]*po[1][1]*po[2][1]*po[3][1]*po[4][1]))) 
+        
+        # flattened_size = flattened_size*2
+        
+        print('flattened_size_compute', flattened_size)
+
+        self.dropout = nn.Dropout(0.5)
+
+        self.fc0 = nn.Sequential(
+            nn.Linear(flattened_size, n_ch_latent),
+            nn.ReLU(),
+            )
+
+    def forward(self, x):
+        x = self.conv0(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.flatn(x)
+        self.dropout(x)
+        x = self.fc0(x)
+        return(x)
+
+
+
+
+
+
+class Decoder(nn.Module):
+    def __init__(self, n_ch_out=3, n_ch_latent=256, shape_output = (32, 32), n_conv_blocks=4,
+                 ch = [256, 128, 64, 32, 16], po = [(2, 2), (2, 2), (2, 2), (2, 2), (2, 2)]):
+        
+        super().__init__()
+
+        self.n_conv_blocks = n_conv_blocks
+        self.sho = shape_output
+        self.chout = n_ch_out
+
+        if self.n_conv_blocks == 5:
+            aa = self.sho[0] // (po[0][0]*po[1][0]*po[2][0]*po[3][0]*po[4][0])
+            bb = self.sho[1] // (po[0][1]*po[1][1]*po[2][1]*po[3][1]*po[4][1])
+            last_ch_depth = ch[0]
+        if self.n_conv_blocks == 4:
+            aa = self.sho[0] // (po[0][0]*po[1][0]*po[2][0]*po[3][0])
+            bb = self.sho[1] // (po[0][1]*po[1][1]*po[2][1]*po[3][1])
+            last_ch_depth = ch[0]
+        if self.n_conv_blocks == 3:
+            aa = self.sho[0] // (po[1][0]*po[2][0]*po[3][0])
+            bb = self.sho[1] // (po[1][1]*po[2][1]*po[3][1])
+            last_ch_depth = ch[1]
+        if self.n_conv_blocks == 2:
+            aa = self.sho[0] // (po[2][0]*po[3][0])
+            bb = self.sho[1] // (po[2][1]*po[3][1])
+            last_ch_depth = ch[2]
+        if self.n_conv_blocks == 1:
+            aa = self.sho[0] // (po[3][0])
+            bb = self.sho[1] // (po[3][1])
+            last_ch_depth = ch[3]
+        if self.n_conv_blocks == 0:
+            aa = self.sho[0] 
+            bb = self.sho[1]
+            last_ch_depth = self.chout
+            
+        flattened_size = aa * bb * last_ch_depth # ch[0]
+        print('flattened_size', flattened_size)
+        # rewrap array into a shape for conv
+        cd1 = flattened_size // (aa*last_ch_depth)
+        cd2 = flattened_size // (bb*last_ch_depth)
+       
+        self.linear = nn.Sequential(
+            nn.Linear(n_ch_latent, flattened_size),
+            nn.ReLU(),
+            )
+        
+        print('reshape to: ', last_ch_depth, cd2, cd1)
+        self.unfla = nn.Unflatten(1, (last_ch_depth, cd2, cd1))
+
+        # transpose conv block 0
+        if self.n_conv_blocks >= 5:
+            self.tconv0 = nn.Sequential(
+                nn.ConvTranspose2d(ch[0], ch[0], kernel_size=(3,3), padding=1),
+                nn.ConvTranspose2d(ch[0], ch[0], kernel_size=(3,3), padding=1, stride=po[0], output_padding=1), 
+                nn.BatchNorm2d(ch[0]),
+                nn.ReLU()
+                )
+        else:
+            self.tconv0 = nn.Identity()
+
+        # transpose conv block 1
+        if self.n_conv_blocks >= 4:
+            self.tconv1 = nn.Sequential(
+                nn.ConvTranspose2d(ch[0], ch[1], kernel_size=(3,3), padding=1),
+                nn.ConvTranspose2d(ch[1], ch[1], kernel_size=(3,3), padding=1, stride=po[1], output_padding=1), 
+                nn.BatchNorm2d(ch[1]),
+                nn.ReLU()
+                )
+        else:
+            self.tconv1 = nn.Identity()
+        
+        # transpose conv block 2
+        if self.n_conv_blocks >= 3:
+            self.tconv2 = nn.Sequential(
+                nn.ConvTranspose2d(ch[1], ch[2], kernel_size=(3,3), padding=1),
+                nn.ConvTranspose2d(ch[2], ch[2], kernel_size=(3,3), padding=1, stride=po[2], output_padding=1), 
+                nn.BatchNorm2d(ch[2]),
+                nn.ReLU()
+                )
+        else:
+            self.tconv2 = nn.Identity()
+
+        # transpose conv block 3
+        if self.n_conv_blocks >= 2:
+            self.tconv3 = nn.Sequential(
+                nn.ConvTranspose2d(ch[2], ch[3], kernel_size=(3,3), padding=1),
+                nn.ConvTranspose2d(ch[3], ch[3], kernel_size=(3,3), padding=1, stride=po[3], output_padding=1), 
+                nn.BatchNorm2d(ch[3]),
+                nn.ReLU()
+                )
+        else:
+            self.tconv3 = nn.Identity()
+        
+       # transpose conv block 4
+        if self.n_conv_blocks >= 1:
+            self.tconv4 = nn.Sequential(
+                nn.ConvTranspose2d(ch[3], ch[4], kernel_size=(3,3), padding=1),
+                nn.ConvTranspose2d(ch[4], ch[4], kernel_size=(3,3), padding=1, stride=po[4], output_padding=1), 
+                nn.BatchNorm2d(ch[4]),
+                nn.ReLU()
+                )
+        else:
+            self.tconv4 = nn.Identity()
+
+        if self.n_conv_blocks >= 1:
+            self.out_map = nn.Sequential(
+                nn.Conv2d(ch[4], self.chout, kernel_size=(1,1), padding=0),
+                nn.Sigmoid()
+                )
+        else:
+            self.out_map = nn.Sequential(
+                nn.Conv2d(self.chout, self.chout, kernel_size=(1,1), padding=0),
+                nn.Sigmoid()
+                )
+
+        # self.relu = nn.ReLU()
+     
+    def forward(self, x):
+        x = self.linear(x)
+        x = self.unfla(x)
+        x = self.tconv0(x)
+        x = self.tconv1(x)
+        x = self.tconv2(x)
+        x = self.tconv3(x)
+        x = self.tconv4(x)
+        x = self.out_map(x)
+        return x
+
+
+
+
+# devel code - supress execution if this is imported as module 
+if __name__ == "__main__":
+
+
+    impsha = (128, 128)
+    latsha = 256
+    n_blck = 3
+
+    model_enc = Encoder(n_ch_in = 1, 
+                        n_ch_latent=latsha, 
+                        shape_input = impsha, 
+                        n_conv_blocks = n_blck,
+                        ch = [16, 32, 64, 256, 512],
+                        po = [(2, 2), (2, 2), (2, 2), (2, 2), (2, 2)]
+                        ) 
+    model_enc = model_enc.to(device)
+    summary(model_enc, (1, impsha[0], impsha[1]))
+
+
+
+
+    model_dec = Decoder(n_ch_out = 1, 
+                        n_ch_latent=latsha, 
+                        shape_output = impsha, 
+                        n_conv_blocks = n_blck,
+                        ch = [128, 64, 64, 32, 32],
+                        po = [(2, 2), (2, 2), (2, 2), (2, 2), (2, 2)]
+                        )
+    model_dec = model_dec.to(device)
+    summary(model_dec, (latsha,))
+
+
+
+
+
+
+
+
