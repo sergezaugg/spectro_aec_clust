f89e4d54fac1e5c21d6a5a38093949b75ed523ef
Serge Zaugg <serge.zaugg@gmail.com>
2025-05-07 11:55:15 +0200

    misc

--- 01_train_aec.py
+++ 01_train_aec.py
@@ -26,7 +26,8 @@ imgpath_test  = "D:/xc_real_projects/xc_sw_europe/images_24000sps_20250406_09252
 
 model_path = "D:/xc_real_projects/models"
 
-batch_size = 8
+batch_size_tr = 8
+batch_size_te = 32
 
 n_epochs = 40
 
@@ -50,25 +51,33 @@ par = {
 
 #----------------------
 # define data loader 
-train_dataset = SpectroImageDataset(imgpath_train, par = par , augment_1 = True, denoise_1 = False,    augment_2 = True, denoise_2 = True)
+train_dataset = SpectroImageDataset(imgpath_train, par = par, augment_1 = True, denoise_1 = False, augment_2 = True, denoise_2 = True)
+train_loader  = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size_tr,  shuffle=True, drop_last=True)
+
+test_dataset  = SpectroImageDataset(imgpath_test, par = None, augment_1 = False, denoise_1 = False, augment_2 = False, denoise_2 = False)
+test_loader   = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size_te,  shuffle=True, drop_last=True)
 
 
+# get some info 
 train_dataset.__len__()
-xx = train_dataset.__getitem__(45)
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,  shuffle=True, drop_last=True)
-for i, (da_orig, data_augm, fi) in enumerate(train_loader, 0):
-    if i > 2:
-        break
-    print(data_augm.shape)
-    print(da_orig.shape)
-
-n_batches = train_dataset.__len__() // batch_size
-n_batches
-
-test_dataset = SpectroImageDataset(imgpath_test, par = par , augment_1 = False, denoise_1 = False,    augment_2 = False, denoise_2 = True)
-
 test_dataset.__len__()
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,  shuffle=True, drop_last=True)
+tra_itm = train_dataset.__getitem__(45)
+tes_itm = test_dataset.__getitem__(45)
+tra_itm[0].shape
+tra_itm[1].shape
+tes_itm[0].shape
+tes_itm[1].shape
+
+
+
+
+
+
+n_batches_tr = train_dataset.__len__() // batch_size_tr
+n_batches_tr
+
+n_batches_te = test_dataset.__len__() // batch_size_te
+n_batches_te
 
 #----------------------
 # define untrained models 
@@ -85,17 +94,11 @@ if False:
     model_enc.load_state_dict(torch.load(os.path.join(model_path, path_enc), weights_only=True))
     model_dec.load_state_dict(torch.load(os.path.join(model_path, path_dec), weights_only=True))
 
-# encoder_model_20250418_223302_epo_14.pth
-# encoder_model_20250418_223302_epo_14.pth
-
 
 model_enc = model_enc.to(device)
-# summary(model_enc, (1, 128, 128))
 summary(model_enc, (1, 128, 1152))
 
 model_dec = model_dec.to(device)
-# summary(model_dec, (512, 1, 8))
-# summary(model_dec, (512, 1, 72))
 summary(model_dec, (128, 1, 36))
 
 # instantiate loss, optimizer
@@ -107,62 +110,80 @@ mse_test_li = []
 mse_trai_li = []
 for epoch in range(n_epochs):
     print(f"Epoch: {epoch + 1}/{n_epochs}")
+
+    #----------------
+    # Train the model 
     # set the encoder and decoder models to training mode
-    loss_tra =[]
     _ = model_enc.train()
     _ = model_dec.train()
     trai_perf_li = []
-    for btchi, (da_orig, data_augm, fi) in enumerate(train_loader, 0):
-        if btchi > 700:
-            break
-        # print(btchi)
-        data_augm = data_augm.to(device)
-        da_orig = da_orig.to(device)
+    for batch_tr, (da_tr_1, da_tr_2, fi) in enumerate(train_loader, 0):
+        da_tr_1 = da_tr_1.to(device)
+        da_tr_2 = da_tr_2.to(device)
         # reset the gradients 
         optimizer.zero_grad()
         # forward 
-        encoded = model_enc(data_augm)#.to(device)
+        encoded = model_enc(da_tr_1)
         # encoded.shape
-        decoded = model_dec(encoded)#.to(device)
+        decoded = model_dec(encoded)
         # compute the reconstruction loss 
-        loss = criterion(decoded, da_orig)
+        loss = criterion(decoded, da_tr_2)
         trai_perf_li.append(loss.cpu().detach().numpy().item())
         # compute the gradients
         loss.backward()
         # update the weights
         optimizer.step()
-
-        if btchi % 10 == 0:
-            print('loss', np.round(loss.item(),5), "   --- status: "  + str(btchi) + " out of " + str(n_batches) + " batches")
+        # feedback every 10th batch
+        if batch_tr % 10 == 0:
+            print('loss', np.round(loss.item(),5), " --- "  + str(batch_tr) + " out of " + str(n_batches_tr) + " batches")
             print(decoded.cpu().detach().numpy().min().round(3) , decoded.cpu().detach().numpy().max().round(3) )
             print("-")
     mse_trai_li.append(np.array(trai_perf_li).mean())        
+    #----------------
 
 
-    # Testing the model
+
+
+
+
+
+
+
+    #----------------------------------
+    # Testing the model at end of epoch 
     _ = model_enc.eval()
     _ = model_dec.eval()
     with torch.no_grad():
         test_perf_li = []
-        for btchi, (da_orig, data_augm, fi) in enumerate(test_loader, 0):
-            if btchi > 20:
-                break
-             # print(btchi)
-            data_augm = data_augm.to(device)
-            da_orig = da_orig.to(device)
+        for btchi, (da_te_1, da_te_2, fi) in enumerate(test_loader, 0):
+            if btchi > 100: break
+            da_te_1 = da_te_1.to(device)
+            da_te_2 = da_te_2.to(device)
             # forward 
-            encoded = model_enc(data_augm)#.to(device)
+            encoded = model_enc(da_te_1)#.to(device)
             # encoded.shape
             decoded = model_dec(encoded)#.to(device)
             # compute the reconstruction loss 
-            loss_test = criterion(decoded, da_orig)
+            loss_test = criterion(decoded, da_te_2)
             test_perf_li.append(loss_test.cpu().detach().numpy().item())
+            # feedback every 10th batch
             if btchi % 10 == 0:
-                print('TEST loss', np.round(loss_test.item(),5), "   --- status: "  + str(btchi) )
+                print('TEST loss', np.round(loss_test.item(),5), " --- "  + str(btchi) + " out of " + str(n_batches_te) + " batches")
         mse_test_li.append(np.array(test_perf_li).mean())
-           
+    #----------------------------------
+      
 
   
+
+
+
+
+
+
+
+
+
+
 
 # reshape performance metrics to a neat lil df
 mse_test = np.array(mse_test_li)
@@ -201,31 +222,6 @@ if True:
     df_mse.to_pickle(path=os.path.join(model_path, mse_save_name))
 
 
-# check reconstruction with examples 
-if True:
-
-    _ = model_enc.eval()
-    _ = model_dec.eval()
-
-    data = da_orig.to(device)
-    encoded = model_enc(data).to(device)
-    decoded = model_dec(encoded).to(device)
-
-    # ii = 489 
-    for ii in np.random.randint(data.shape[0], size = 15):
-        img_orig = data[ii].cpu().numpy()
-        img_orig = img_orig.squeeze() # 1 ch
-        img_orig = 255*(img_orig - img_orig.min())/(img_orig.max())
-        # fig00 = px.imshow(img_orig, height = 500, title="original")
-        img_reco = decoded[ii].cpu().detach().numpy()
-        img_reco = img_reco.squeeze()  # 1 ch
-        img_reco = 255*(img_reco - img_reco.min())/(img_reco.max())
-        # fig01 = px.imshow(img_reco, height = 500, title="reconstructed")
-        fig = make_subplots(rows=1, cols=2)
-        _ = fig.add_trace(px.imshow(img_orig).data[0], row=1, col=1)
-        _ = fig.add_trace(px.imshow(img_reco).data[0], row=1, col=2)
-        _ = fig.update_layout(autosize=True,height=550, width = 1000)
-        fig.show()
 
 
 
