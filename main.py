ac4ed71e4a777f8ce205ff4000414413e72aa814
Serge Zaugg <serge.zaugg@gmail.com>
2025-05-14 17:29:23 +0200

    train session params now stored in json files

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Description : 
 #--------------------------------
 
+import os
+import json
 import plotly.express as px
 import torch
 from torchsummary import summary
@@ -12,73 +14,25 @@ from utils import evaluate_reconstruction_on_examples, encoder_based_feature_ext
 torch.cuda.is_available()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-
-
-
 #----------------------------------------------------------------------
 # (1) train 
 
-
-sess_info = {
-    'path_untrained_models' : "D:/xc_real_projects/untrained_models",
-    'path_trained_models' : "D:/xc_real_projects/trained_models",
-    'imgpath_train' : "D:/xc_real_projects/xc_ne_europe/images_24000sps_20250406_095331",
-    'imgpath_test'  : "D:/xc_real_projects/xc_sw_europe/images_24000sps_20250406_092522",
-    'batch_size_tr' : 8,  # 8,
-    'batch_size_te' : 32,
-    'n_epochs' : 10,
-
-    # 'hot_start' : False, 
-    # 'model_tag' : 'gen_B0',  
-
-    'hot_start' : True, 
-    'model_tag' : '20250513_001557',  
-
-    # 
-    'data_generator' : {
-        'da': {
-            'rot_deg'    : 0.30,  # ok
-            'trans_prop' : 0.005, # ok
-            'brightness' : 0.40,  # ok
-            'contrast'   : 0.40,  # ok
-            'gnoisesigm' : 0.10,  # ok
-            'gnoiseprob' : 0.50,  # ok
-            },
-        'den': {  
-        'thld' :  0.40, #  0.30, 
-            } 
-        }
-    }
-
-
-
-
+with open(os.path.join('./session_params', 'sess_02.json' )) as f:
+    sess_info = json.load(f)
 
 train_dataset = SpectroImageDataset(sess_info['imgpath_train'], par = sess_info['data_generator'], augment_1 = True, denoise_1 = False, augment_2 = False, denoise_2 = True)
 test_dataset  = SpectroImageDataset(sess_info['imgpath_test'],  par = sess_info['data_generator'], augment_1 = False, denoise_1 = False, augment_2 = False, denoise_2 = True)
 
 fig01 = make_data_augment_examples(pt_dataset = train_dataset, batch_size = 16)
 fig01.show()
-
 fig02 = make_data_augment_examples(pt_dataset = test_dataset, batch_size = 16)
 fig02.show()
 
-
 model_enc, model_dec = get_models(sess_info)
-# model_enc = model_enc.to(device)
 summary(model_enc, (1, 128, 1152))
-# model_dec = model_dec.to(device)
 summary(model_dec, (128, 1, 72))
-summary(model_dec, (128, 1, 144))
 
 train_autoencoder(sess_info, train_dataset, test_dataset, model_enc, model_dec)
-
-
-
-
-
-
-
 
 #----------------------------------------------------------------------
 # (2) evaluate 
@@ -93,8 +47,6 @@ model_list_B0 = ['20250512_002141','20250512_124428','20250513_021206',]
 for tstmp in model_list_B0:
     evaluate_reconstruction_on_examples(path_images = imgpath, path_models = path_trained_models, time_stamp_model = tstmp, n_images = 32,)
 
-
-
 #----------------------------------------------------------------------
 # (3) extract 
 path_images = "D:/xc_real_projects/xc_sw_europe/images_24000sps_20250406_092522"
@@ -105,8 +57,6 @@ di = encoder_based_feature_extraction(path_images, path_models, time_stamp_model
 
 di['feature_array'].shape
 di['image_file_name_array'].shape
-di['path_images']
-di['path_encoder']
 
 wrap_to_dataset(di)
 
@@ -121,14 +71,3 @@ wrap_to_dataset(di)
 
 
 
-
-
-# # get some info 
-# train_dataset.__len__()
-# test_dataset.__len__()
-# tra_itm = train_dataset.__getitem__(45)
-# tes_itm = test_dataset.__getitem__(45)
-# tra_itm[0].shape
-# tra_itm[1].shape
-# tes_itm[0].shape
-# tes_itm[1].shape
