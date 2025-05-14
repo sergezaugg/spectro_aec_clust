885fac8bbd43a0e0c5918824609a910f418f48ef
Serge Zaugg <serge.zaugg@gmail.com>
2025-05-14 17:41:02 +0200

    added mini-model B21 with less conv channels

--- 00_make_models.py
+++ 00_make_models.py
@@ -12,7 +12,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 path_untrained_models = "D:/xc_real_projects/untrained_models"
 
 # load untrained models 
-from custom_models import EncoderGenB0, DecoderGenB0, EncoderGenB1, DecoderGenB1, EncoderGenB2, DecoderGenB2
+from custom_models import EncoderGenB0, DecoderGenB0, EncoderGenB1, DecoderGenB1, EncoderGenB2, DecoderGenB2, EncoderGenB21, DecoderGenB21
 
 # save_file_name = "_gen_B0"
 # model_enc = EncoderGenB0()
@@ -22,9 +22,14 @@ from custom_models import EncoderGenB0, DecoderGenB0, EncoderGenB1, DecoderGenB1
 # model_enc = EncoderGenB1()
 # model_dec = DecoderGenB1()
 
-save_file_name = "_gen_B2"
-model_enc = EncoderGenB2()
-model_dec = DecoderGenB2()
+# save_file_name = "_gen_B2"
+# model_enc = EncoderGenB2()
+# model_dec = DecoderGenB2()
+
+save_file_name = "_gen_B21"
+model_enc = EncoderGenB21()
+model_dec = DecoderGenB21()
+
 
 model_enc = model_enc.to(device)
 model_dec = model_dec.to(device)
@@ -33,7 +38,7 @@ model_dec = model_dec.to(device)
 summary(model_enc, (1, 128, 1152))
 summary(model_dec, (128, 1, 36))
 summary(model_dec, (128, 1, 72))
-summary(model_dec, (128, 1, 144))
+summary(model_dec, (64, 1, 144))
 
 # save for later use 
 torch.save(model_enc, os.path.join(path_untrained_models, 'cold_encoder' + save_file_name + '.pth'))
--- main.py
+++ main.py
@@ -8,30 +8,23 @@ import json
 import plotly.express as px
 import torch
 from torchsummary import summary
-from utils import SpectroImageDataset, make_data_augment_examples
-from utils import get_models, train_autoencoder
+from utils import SpectroImageDataset, make_data_augment_examples, get_models, train_autoencoder
 from utils import evaluate_reconstruction_on_examples, encoder_based_feature_extraction, wrap_to_dataset
 torch.cuda.is_available()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 #----------------------------------------------------------------------
 # (1) train 
-
 with open(os.path.join('./session_params', 'sess_02.json' )) as f:
     sess_info = json.load(f)
 
 train_dataset = SpectroImageDataset(sess_info['imgpath_train'], par = sess_info['data_generator'], augment_1 = True, denoise_1 = False, augment_2 = False, denoise_2 = True)
 test_dataset  = SpectroImageDataset(sess_info['imgpath_test'],  par = sess_info['data_generator'], augment_1 = False, denoise_1 = False, augment_2 = False, denoise_2 = True)
-
-fig01 = make_data_augment_examples(pt_dataset = train_dataset, batch_size = 16)
-fig01.show()
-fig02 = make_data_augment_examples(pt_dataset = test_dataset, batch_size = 16)
-fig02.show()
-
+make_data_augment_examples(pt_dataset = train_dataset, batch_size = 16).show()
+fig02 = make_data_augment_examples(pt_dataset = test_dataset, batch_size = 16).show()
 model_enc, model_dec = get_models(sess_info)
 summary(model_enc, (1, 128, 1152))
 summary(model_dec, (128, 1, 72))
-
 train_autoencoder(sess_info, train_dataset, test_dataset, model_enc, model_dec)
 
 #----------------------------------------------------------------------
@@ -39,13 +32,11 @@ train_autoencoder(sess_info, train_dataset, test_dataset, model_enc, model_dec)
 imgpath ="D:/xc_real_projects/example_images/rectangular_1"
 n_images = 32
 path_trained_models = "D:/xc_real_projects/trained_models"
-
 model_list_B2 = ['20250511_205505','20250512_025001','20250512_215905','20250513_230102',]
 model_list_B1 = ['20250511_231810','20250512_100933','20250513_001557', '20250514_013412']
 model_list_B0 = ['20250512_002141','20250512_124428','20250513_021206',]
-
 for tstmp in model_list_B0:
-    evaluate_reconstruction_on_examples(path_images = imgpath, path_models = path_trained_models, time_stamp_model = tstmp, n_images = 32,)
+    evaluate_reconstruction_on_examples(path_images = imgpath, path_models = path_trained_models, time_stamp_model = tstmp, n_images = 32)
 
 #----------------------------------------------------------------------
 # (3) extract 
@@ -54,10 +45,8 @@ path_models = "D:/xc_real_projects/trained_models"
 time_stamp_model = '20250512_215905'
 
 di = encoder_based_feature_extraction(path_images, path_models, time_stamp_model, devel = True)
-
 di['feature_array'].shape
 di['image_file_name_array'].shape
-
 wrap_to_dataset(di)
 
 
