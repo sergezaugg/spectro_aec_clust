06ca892dc7b46986ee4a056508afda53cf2366dc
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-08 09:26:24 +0200

    re-aligned code

--- main_02_extract.py
+++ main_02_extract.py
@@ -7,11 +7,19 @@ import os
 import json
 import numpy as np
 import torch
-from utils import SpectroImageDataset
-from utils import encoder_based_feature_extraction, dim_reduce
+from utils import encoder_based_feature_extraction, dim_reduce, evaluate_reconstruction_on_examples
 torch.cuda.is_available()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+
+
+
+#----------------------
+# (2) evaluate reconstruction
+imgpath ="D:/xc_real_projects/xc_sw_europe/xc_spectrograms"
+path_trained_models = "D:/xc_real_projects/trained_models"
+tstmp = '20250607_173742'
+evaluate_reconstruction_on_examples(path_images = imgpath, path_models = path_trained_models, time_stamp_model = tstmp, n_images = 32)
 
 
 #----------------------------------------------------------------------
