b91cb723f66b91b92bc027b69e8ca8b2771e440b
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-10 08:40:22 +0200

    changes in main_02

--- main_02_extract.py
+++ main_02_extract.py
@@ -8,14 +8,14 @@ from utils import AutoencoderExtract
 torch.cuda.is_available()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# path_images = "D:/xc_real_projects/xc_sw_europe/xc_spectrograms"
-path_images = "D:/xc_real_projects/xc_parus_01/xc_spectrograms"
+path_images = "D:/xc_real_projects/xc_sw_europe/xc_spectrograms"
+# path_images = "D:/xc_real_projects/xc_parus_01/xc_spectrograms"
 
 path_models = "D:/xc_real_projects/trained_models"
 # time_stamp_model = '20250608_155900' # gen_B0 old training set
 # time_stamp_model = '20250609_025355' # gen_B0 new big training set
 # time_stamp_model = '20250609_135101' # gen_B1 new big training set
-time_stamp_model = '20250609_212853' # gen_B0L (large) new big training set
+time_stamp_model = '20250610_002647' # gen_B0L (large) new big training set
 
 
 
@@ -24,7 +24,7 @@ time_stamp_model = '20250609_212853' # gen_B0L (large) new big training set
 ae = AutoencoderExtract(path_images, path_models, time_stamp_model)
 
 # evaluate reconstruction
-ae.evaluate_reconstruction_on_examples(n_images = 32, shuffle = False).show()
+ae.evaluate_reconstruction_on_examples(n_images = 32, shuffle = True).show()
 
 # extract (will save to disk as npz in parent dir of path_models)
 ae.encoder_based_feature_extraction(devel = False)
