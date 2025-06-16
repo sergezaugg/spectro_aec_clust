d6adf69166b694299cbeaaa3c9b08bfe1fd81426
Serge Zaugg <serge.zaugg@gmail.com>
2025-06-16 16:45:05 +0200

    interim

--- main_00_make.py
+++ main_00_make.py
@@ -13,13 +13,51 @@ with open('./config/config.yaml') as f:
     conf = yaml.safe_load(f)
 path_untrained_models = conf['path_untrained_models']
 
-from model_collection.model_collection import EncoderGenBTP08 as Encoder
-from model_collection.model_collection import DecoderGenBTP08 as Decoder
-save_file_name = "_GenBTP08_CH0256"
+
+
+
+# REFERENCE model 
+from model_collection.model_collection import EncoderGenBTP32 as Encoder
+from model_collection.model_collection import DecoderGenBTP32 as Decoder
+save_file_name = "_GenBTP32_CH0256"
 model_enc = Encoder(n_ch_in = 3, ch = [64, 128, 128, 128, 256])
 model_dec = Decoder(n_ch_out = 3, ch = [256, 128, 128, 128, 64])
-summary(model_enc, (1, 3, 128, 1152))
-summary(model_dec, (1, 256, 1, 144))
+summary(model_enc, (1, 3, 128, 1152), depth = 1)
+summary(model_dec, (1, 256, 1, 36), depth = 1)
+torch.save(model_enc, os.path.join(path_untrained_models, 'cold_encoder' + save_file_name + '.pth'))
+torch.save(model_dec, os.path.join(path_untrained_models, 'cold_decoder' + save_file_name + '.pth'))
+
+
+
+# new ----- without trans conv
+from model_collection.model_collection import EncoderGenCTP32 as Encoder
+from model_collection.model_collection import DecoderGenCTP32 as Decoder
+save_file_name = "_GenC_new_TP32_CH0256"
+model_enc = Encoder(n_ch_in = 3, n_ch_out = 256, ch = [64, 128, 128, 128])
+model_dec = Decoder(n_ch_in = 256, n_ch_out = 3, ch = [128, 128, 128, 64])
+summary(model_enc, (1, 3, 128, 1152), depth = 1)
+summary(model_dec, (1, 256, 1, 36), depth = 1)
+torch.save(model_enc, os.path.join(path_untrained_models, 'cold_encoder' + save_file_name + '.pth'))
+torch.save(model_dec, os.path.join(path_untrained_models, 'cold_decoder' + save_file_name + '.pth'))
+
+
+
+
+
+
+
+
+
+
+
+
+from model_collection.model_collection import EncoderGenBTP32 as Encoder
+from model_collection.model_collection import DecoderGenBTP32 as Decoder
+save_file_name = "_GenBTP32_CH0512"
+model_enc = Encoder(n_ch_in = 3, ch = [64, 128, 128, 256, 512])
+model_dec = Decoder(n_ch_out = 3, ch = [512, 256, 128, 128, 64])
+summary(model_enc, (1, 3, 128, 1152), depth = 1)
+summary(model_dec, (1, 512, 1, 36), depth = 1)
 torch.save(model_enc, os.path.join(path_untrained_models, 'cold_encoder' + save_file_name + '.pth'))
 torch.save(model_dec, os.path.join(path_untrained_models, 'cold_decoder' + save_file_name + '.pth'))
 
@@ -35,48 +73,20 @@ torch.save(model_enc, os.path.join(path_untrained_models, 'cold_encoder' + save_
 torch.save(model_dec, os.path.join(path_untrained_models, 'cold_decoder' + save_file_name + '.pth'))
 
 
-from model_collection.model_collection import EncoderGenBTP32 as Encoder
-from model_collection.model_collection import DecoderGenBTP32 as Decoder
-save_file_name = "_GenBTP32_CH0256"
+
+
+from model_collection.model_collection import EncoderGenBTP08 as Encoder
+from model_collection.model_collection import DecoderGenBTP08 as Decoder
+save_file_name = "_GenBTP08_CH0256"
 model_enc = Encoder(n_ch_in = 3, ch = [64, 128, 128, 128, 256])
 model_dec = Decoder(n_ch_out = 3, ch = [256, 128, 128, 128, 64])
 summary(model_enc, (1, 3, 128, 1152))
-summary(model_dec, (1, 256, 1, 36))
+summary(model_dec, (1, 256, 1, 144))
 torch.save(model_enc, os.path.join(path_untrained_models, 'cold_encoder' + save_file_name + '.pth'))
 torch.save(model_dec, os.path.join(path_untrained_models, 'cold_decoder' + save_file_name + '.pth'))
 
 
-from model_collection.model_collection import EncoderGenBTP32 as Encoder
-from model_collection.model_collection import DecoderGenBTP32 as Decoder
-save_file_name = "_GenBTP32_CH0512"
-model_enc = Encoder(n_ch_in = 3, ch = [64, 128, 128, 256, 512])
-model_dec = Decoder(n_ch_out = 3, ch = [512, 256, 128, 128, 64])
-summary(model_enc, (1, 3, 128, 1152), depth = 1)
-summary(model_dec, (1, 512, 1, 36), depth = 1)
-torch.save(model_enc, os.path.join(path_untrained_models, 'cold_encoder' + save_file_name + '.pth'))
-torch.save(model_dec, os.path.join(path_untrained_models, 'cold_decoder' + save_file_name + '.pth'))
 
-
-from model_collection.model_collection import EncoderGenBTP64 as Encoder
-from model_collection.model_collection import DecoderGenBTP64 as Decoder
-save_file_name = "_GenBTP64_CH0256"
-model_enc = Encoder(n_ch_in = 3, ch = [64, 128, 128, 128, 256])
-model_dec = Decoder(n_ch_out = 3, ch = [256, 128, 128, 128, 64])
-summary(model_enc, (1, 3, 128, 1152))
-summary(model_dec, (1, 256, 1, 18))
-torch.save(model_enc, os.path.join(path_untrained_models, 'cold_encoder' + save_file_name + '.pth'))
-torch.save(model_dec, os.path.join(path_untrained_models, 'cold_decoder' + save_file_name + '.pth'))
-
-
-from model_collection.model_experiments import EncoderGenCTP128 as Encoder
-from model_collection.model_experiments import DecoderGenCTP128 as Decoder
-save_file_name = "_GenC_experiment"
-model_enc = Encoder(n_ch_in = 3, n_ch_out = 256, ch = [64, 128, 128, 256])
-model_dec = Decoder(n_ch_in = 256, n_ch_out = 3, ch = [256, 128, 128, 64])
-summary(model_enc, (1, 3, 128, 1152), depth = 1)
-summary(model_dec, (1, 256, 1, 36), depth = 1)
-torch.save(model_enc, os.path.join(path_untrained_models, 'cold_encoder' + save_file_name + '.pth'))
-torch.save(model_dec, os.path.join(path_untrained_models, 'cold_decoder' + save_file_name + '.pth'))
 
 
 
--- model_collection/model_experiments.py
+++ model_collection/model_experiments.py
@@ -7,186 +7,12 @@ import torch
 import torch.nn as nn
 from torchinfo import summary
 
-
-# -------------------------------------------------
-# GEN C (freq-pool : 128 - time-pool : 128)
-
-class EncoderGenCTP128(nn.Module):
-    def __init__(self, n_ch_in = 3, n_ch_out=256,
-                 ch = [64, 64, 64, 128, 128, 128]
-                 ):
-        super().__init__()
-        po = [(4, 2), (4, 2), (2, 2), (2, 2), (2, 2)]
-        self.padding =  "same"
-        conv_kernel = (3,3)
-        self.conv0 = nn.Sequential(
-            nn.Conv2d(n_ch_in,  ch[0], kernel_size=conv_kernel, stride=1, padding=self.padding),
-            nn.BatchNorm2d(ch[0]),
-            nn.ReLU(),
-            nn.Conv2d(ch[0],  ch[0], kernel_size=conv_kernel, stride=1, padding=self.padding),
-            nn.BatchNorm2d(ch[0]),
-            nn.ReLU(),
-            nn.AvgPool2d(po[0], stride=po[0]))
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(ch[0], ch[1], kernel_size=conv_kernel, stride=1, padding=self.padding),
-            nn.BatchNorm2d(ch[1]),
-            nn.ReLU(),
-            nn.Conv2d(ch[1], ch[1], kernel_size=conv_kernel, stride=1, padding=self.padding),
-            nn.BatchNorm2d(ch[1]),
-            nn.ReLU(),
-            nn.AvgPool2d(po[1], stride=po[1]))
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(ch[1], ch[2], kernel_size=conv_kernel, stride=1, padding=self.padding),
-            nn.BatchNorm2d(ch[2]),
-            nn.ReLU(),
-            nn.Conv2d(ch[2], ch[2], kernel_size=conv_kernel, stride=1, padding=self.padding),
-            nn.BatchNorm2d(ch[2]),
-            nn.ReLU(),
-            nn.AvgPool2d(po[2], stride=po[2]))
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(ch[2], ch[3], kernel_size=conv_kernel, stride=1, padding=self.padding),
-            nn.BatchNorm2d(ch[3]),
-            nn.ReLU(),
-            nn.Conv2d(ch[3], ch[3], kernel_size=conv_kernel, stride=1, padding=self.padding),
-            nn.BatchNorm2d(ch[3]),
-            nn.ReLU(),
-            nn.AvgPool2d(po[3], stride=po[3]))
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(ch[3], ch[3], kernel_size=conv_kernel, stride=1, padding=self.padding),
-            nn.BatchNorm2d(ch[3]),
-            nn.ReLU(),
-            nn.Conv2d(ch[3], n_ch_out, kernel_size=conv_kernel, stride=1, padding=self.padding),
-            nn.BatchNorm2d(n_ch_out),
-            nn.ReLU(),
-            nn.AvgPool2d(po[4], stride=po[4]))
-        
-    def forward(self, x):
-        x = self.conv0(x)
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        return(x)
-
-
-class DecoderGenCTP128(nn.Module):
-    def __init__(self, n_ch_in=256, n_ch_out=3, 
-                 ch = [128, 128, 128, 64, 64, 64]):
-        super().__init__()
-        self.padding =  "same"
-        po =  [(2, 2), (2, 2), (2, 2), (4, 2), (4, 2)]
-        self.tconv0 = nn.Sequential(
-            nn.Upsample(scale_factor=po[0], mode='bilinear'),
-            nn.Conv2d(n_ch_in,  ch[0], kernel_size=(3,3), stride=1, padding=self.padding),
-            nn.BatchNorm2d(ch[0]),
-            nn.ReLU(),
-            nn.Conv2d(ch[0],  ch[0], kernel_size=(3,3), stride=1, padding=self.padding),
-            nn.BatchNorm2d(ch[0]),
-            nn.ReLU(),
-            )
-        self.tconv1 = nn.Sequential(
-            nn.Upsample(scale_factor=po[1], mode='bilinear'),
-            nn.Conv2d(ch[0], ch[1], kernel_size=(3,3), stride=1, padding=self.padding), 
-            nn.BatchNorm2d(ch[1]),
-            nn.ReLU(),
-            nn.Conv2d(ch[1], ch[1], kernel_size=(3,3), stride=1, padding=self.padding), 
-            nn.BatchNorm2d(ch[1]),
-            nn.ReLU(),
-            )
-        self.tconv2 = nn.Sequential(
-            nn.Upsample(scale_factor=po[2], mode='bilinear'),
-            nn.Conv2d(ch[1], ch[2], kernel_size=(3,3), stride=1, padding=self.padding),
-            nn.BatchNorm2d(ch[2]),
-            nn.ReLU(),
-            nn.Conv2d(ch[2], ch[2], kernel_size=(3,3), stride=1, padding=self.padding),    
-            nn.BatchNorm2d(ch[2]),
-            nn.ReLU(),
-            )
-        self.tconv3 = nn.Sequential(
-            nn.Upsample(scale_factor=po[3], mode='bilinear'),
-            nn.Conv2d(ch[2], ch[3], kernel_size=(3,3), stride=1, padding=self.padding), 
-            nn.BatchNorm2d(ch[3]),
-            nn.ReLU(),
-            nn.Conv2d(ch[3], ch[3], kernel_size=(3,3), stride=1, padding=self.padding), 
-            nn.BatchNorm2d(ch[3]),
-            nn.ReLU(),
-            )
-        self.out_map = nn.Sequential(
-            nn.Upsample(scale_factor=po[4], mode='bilinear'),
-            nn.Conv2d(ch[3], ch[3], kernel_size=(3,3), stride=1, padding=self.padding), 
-            nn.BatchNorm2d(ch[3]),
-            nn.ReLU(),
-            nn.Conv2d(ch[3], n_ch_out, kernel_size=(3,3), stride=1, padding=self.padding), 
-            nn.BatchNorm2d(n_ch_out),
-            nn.Sigmoid(),
-            )
-
-    def forward(self, x):
-        x = self.tconv0(x)
-        x = self.tconv1(x)
-        x = self.tconv2(x)
-        x = self.tconv3(x)
-        x = self.out_map(x)
-        return x
-
-# -------------------------------------------------
-
-
-
-
 if __name__ == "__main__":
 
-    model_enc = EncoderGenCTP128(n_ch_in = 3, n_ch_out = 256, ch = [64, 128, 128, 256])
-    model_dec = DecoderGenCTP128(n_ch_in = 256, n_ch_out = 3, ch = [256, 128, 128, 64])
-    
-    summary(model_enc, (1, 3, 128, 1152), depth = 1)
-    summary(model_dec, (1, 256, 1, 36), depth = 1)
-
-
-    
-    # # from copilot 
-    # class AvgPoolUpsampleSpectrogramAutoencoder(nn.Module):
-    #     def __init__(self):
-    #         super(AvgPoolUpsampleSpectrogramAutoencoder, self).__init__()
-    #         # Encoder with AvgPool2d
-    #         self.encoder = nn.Sequential(
-    #             nn.Conv2d(1, 16, kernel_size=3, padding=1),     # [B, 1, H, W] -> [B, 16, H, W]
-    #             nn.ReLU(True),
-    #             nn.AvgPool2d(kernel_size=2, stride=2),          # [B, 16, H/2, W/2]
-    #             nn.Conv2d(16, 32, kernel_size=3, padding=1),    # [B, 32, H/2, W/2]
-    #             nn.ReLU(True),
-    #             nn.AvgPool2d(kernel_size=2, stride=2),          # [B, 32, H/4, W/4]
-    #             nn.Conv2d(32, 64, kernel_size=3, padding=1),    # [B, 64, H/4, W/4]
-    #             nn.ReLU(True),
-    #             nn.AvgPool2d(kernel_size=2, stride=2)           # [B, 64, H/8, W/8]
-    #         )
-    #         # Decoder with nn.Upsample + nn.Conv2d
-    #         self.decoder = nn.Sequential(
-    #             nn.Upsample(scale_factor=2, mode='nearest'),    # [B, 64, H/4, W/4]
-    #             nn.Conv2d(64, 32, kernel_size=3, padding=1),
-    #             nn.ReLU(True),
-    #             nn.Upsample(scale_factor=2, mode='nearest'),    # [B, 32, H/2, W/2]
-    #             nn.Conv2d(32, 16, kernel_size=3, padding=1),
-    #             nn.ReLU(True),
-    #             nn.Upsample(scale_factor=2, mode='nearest'),    # [B, 16, H, W]
-    #             nn.Conv2d(16, 1, kernel_size=3, padding=1),
-    #             nn.Sigmoid()  # Use sigmoid if spectrograms are normalized to [0,1]
-    #         )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # model_enc = EncoderGenCTP128(n_ch_in = 3, n_ch_out = 256, ch = [64, 128, 128, 256])
+    # model_dec = DecoderGenCTP128(n_ch_in = 256, n_ch_out = 3, ch = [256, 128, 128, 64])
+    # summary(model_enc, (1, 3, 128, 1152), depth = 1)
+    # summary(model_dec, (1, 256, 1, 36), depth = 1)
 
     class EncoderLSTM_A(nn.Module):
         def __init__(self):
@@ -213,9 +39,6 @@ if __name__ == "__main__":
             out = out.view(-1, out.size(2))
             return out, hidden
 
-
-
-
     summary(
         EncoderLSTM_A(), 
         (1, 128),
@@ -227,9 +50,6 @@ if __name__ == "__main__":
         (1, 100),
         dtypes=[torch.long],
         )
-
-
-
 
     model_enc = EncoderLSTM_A()
 
